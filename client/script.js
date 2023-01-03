a1d41c603fb0311d4bcbde50d646878c154df236
MateuszEM88 <mateuszem1988@gmail.com>
2023-01-03 20:28:14 +0100

    update server url

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://openai-test.onrender.com/", {
     method: "POST",
     headers: {
       "content-type": "application/json",
